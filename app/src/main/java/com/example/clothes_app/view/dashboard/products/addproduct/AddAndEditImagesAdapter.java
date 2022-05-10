991671295fa5efac01e60de419ee12be64be4b29
Mustafa Hasria <mostafahasria0101@gmail.com>
2022-05-10 11:02:30 +0300

    * Finish pick image in add product * Make some change in add product UI

---
+++ app/src/main/java/com/example/clothes_app/view/dashboard/products/addproduct/AddAndEditImagesAdapter.java
@@ -0,0 +1,99 @@
+package com.example.clothes_app.view.dashboard.products.addproduct;
+
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clothes_app.R;
+import com.example.clothes_app.model.entity.File;
+
+import java.util.List;
+
+public class AddAndEditImagesAdapter extends RecyclerView.Adapter<AddAndEditImagesAdapter.AddAndEditImagesViewHolder>{
+
+    //region Variables
+    List<File> fileList;
+    OnAddAndEditImagesAdapterClickListeners onAddAndEditImagesAdapterClickListeners;
+    //endregion
+
+    //region Constructor
+
+    public AddAndEditImagesAdapter(List<File> fileList, OnAddAndEditImagesAdapterClickListeners onAddAndEditImagesAdapterClickListeners) {
+        this.fileList = fileList;
+        this.onAddAndEditImagesAdapterClickListeners = onAddAndEditImagesAdapterClickListeners;
+    }
+
+    //endregion
+    
+    //region Adapter
+    @NonNull
+    @Override
+    public AddAndEditImagesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recycler_images_product, parent, false);
+        return new AddAndEditImagesViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AddAndEditImagesViewHolder holder, int position) {
+        File currentFile = fileList.get(position);
+        holder.itemRecyclerImagesProductImageView.setImageBitmap(BitmapFactory
+                .decodeByteArray(currentFile.getPicture(), 0, currentFile.getPicture().length));
+    }
+
+    @Override
+    public int getItemCount() {
+        return fileList.size();
+    }
+
+    public void refreshData(List<File> fileList){
+        this.fileList = fileList;
+        notifyDataSetChanged();
+    }
+
+    public void addData(List<File> fileList){
+        this.fileList.addAll(fileList);
+        notifyDataSetChanged();
+    }
+    //endregion
+
+    //region Click listeners
+    public interface OnAddAndEditImagesAdapterClickListeners {
+        void onItemRecyclerImagesProductImageButtonDelete(File file, int position);
+    }
+    //endregion
+
+    //region ViewHolder
+    public class AddAndEditImagesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        //region Components
+        ImageButton itemRecyclerImagesProductImageButtonDelete;
+        ImageView itemRecyclerImagesProductImageView;
+        //endregion
+
+        //region Constructor
+        public AddAndEditImagesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemRecyclerImagesProductImageButtonDelete = itemView.findViewById(R.id.item_recycler_images_product_image_button_delete);
+            itemRecyclerImagesProductImageView = itemView.findViewById(R.id.item_recycler_images_product_image_view);
+            itemRecyclerImagesProductImageButtonDelete.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onAddAndEditImagesAdapterClickListeners.onItemRecyclerImagesProductImageButtonDelete(fileList.get(getAdapterPosition()), getAdapterPosition());
+        }
+
+        //endregion
+    }
+    //endregion
+
+
+}
+
--- app/src/main/java/com/example/clothes_app/view/dashboard/products/addproduct/AddAndEditProductFragment.java
+++ app/src/main/java/com/example/clothes_app/view/dashboard/products/addproduct/AddAndEditProductFragment.java
@@ -1,19 +1,34 @@
 package com.example.clothes_app.view.dashboard.products.addproduct;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.clothes_app.R;
 import com.example.clothes_app.databinding.FragmentAddAndEditProductBinding;
+import com.example.clothes_app.model.entity.File;
 
-public class AddAndEditProductFragment extends Fragment {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddAndEditProductFragment extends Fragment implements AddAndEditImagesAdapter.OnAddAndEditImagesAdapterClickListeners{
 
     //region Variables
     FragmentAddAndEditProductBinding binding;
+    List<File> fileList;
+    AddAndEditImagesAdapter addAndEditImagesAdapter;
     //endregion
 
     //region Life cycle
@@ -23,11 +38,71 @@ public class AddAndEditProductFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_and_edit_product, container, false);
         binding = FragmentAddAndEditProductBinding.bind(view);
-
+        fileList = new ArrayList<>();
+        setupImagesRecyclerView();
         binding.fragmentAddAndEditProductButtonSelectColor.setOnClickListener(view1 -> {
 
         });
+
+        binding.fragmentAddAndEditProductButtonAddImage.setOnClickListener(view1 -> {
+            checkPermissions();
+        });
         return view;
+    }
+    //endregion
+
+    //region Setups
+    void setupImagesRecyclerView() {
+        addAndEditImagesAdapter = new AddAndEditImagesAdapter(fileList, this);
+        binding.fragmentAddAndEditProductRecyclerViewImages.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        binding.fragmentAddAndEditProductRecyclerViewImages.setHasFixedSize(true);
+        binding.fragmentAddAndEditProductRecyclerViewImages.setAdapter(addAndEditImagesAdapter);
+    }
+    //endregion
+
+    //region On activity result
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fileList.add(new File(bitmapToByteArray(bitmap)));
+        addAndEditImagesAdapter.refreshData(fileList);
+    }
+    //endregion
+
+    //region Methods
+
+    private void checkPermissions() {
+        chooseImage();
+    }
+
+    private void chooseImage() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(galleryIntent, "choose picture"), 102);
+    }
+
+    private byte[] bitmapToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+
+    //endregion
+
+    //region Adapter click listeners
+
+    @Override
+    public void onItemRecyclerImagesProductImageButtonDelete(File file, int position) {
+
     }
     //endregion
 
